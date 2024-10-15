9f00bce207725a77a3bc8f54134f29e28b650c93
Jeremiah <44993244+jechton@users.noreply.github.com>
2024-10-14 23:33:52 -0400

    feat(lvin): add lvim config

---
+++ private_dot_config/lvim/config.lua
@@ -0,0 +1,19 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.plugins = {
+  {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.cmd.colorscheme('gruvbox-material')
+    end
+  }
+}
+lvim.colorscheme = "gruvbox-material"
