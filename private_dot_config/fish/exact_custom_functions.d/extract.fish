a9df664c951efc040861f4c4e3505da111c64d96
Jeremiah <44993244+jechton@users.noreply.github.com>
2024-12-27 21:15:18 -0500

    feat(fish): update extract command

--- private_dot_config/fish/exact_custom_functions.d/extract.fish
+++ private_dot_config/fish/exact_custom_functions.d/extract.fish
@@ -1,34 +1,39 @@
 function extract --description "extract file using the right command for the type"
-    if count $argv >/dev/null
-        if test -f $argv
-            switch $argv
-                case \*.rar
-                    unrar x $argv
-                case \*.zip
-                    unzip $argv
-                case \*.tar.bz2
-                    tar xvjf $argv
-                case \*.tar.gz
-                    tar xvzf $argv
-                case \*.bz2
-                    bunzip2 $argv
-                case \*.gz
-                    gunzip $argv
-                case \*.tar
-                    tar xvf $argv
-                case \*.tbz2
-                    tar xvjf $argv
-                case \*.tgz
-                    tar xvzf $argv
-                case \*.Z
-                    uncompress $argv
-                case \*.7z
-                    7z x $argv
-            end
-        else
-            echo "Could not extract this: $argv"
-        end
-    else
-        echo "Usage: extract [FILE]"
+    if not set -q argv[1]
+        echo "Usage: extract <archive_file>" >&2
+        return 1
+    end
+
+    set -l archive_file $argv[1]
+
+    if not test -f $archive_file
+        echo "Error: File '$archive_file' not found." >&2
+        return 1
+    end
+
+    set -l file_type (file --mime-type --brief $archive_file)
+
+    switch "$file_type"
+        case application/x-rar-compresseed
+            unrar x $archive_file
+        case application/zip
+            unzip $archive_file
+        case application/x-tar
+            tar xvf $archive_file
+        case application/x-bzip2
+            tar xvjf $archive_file # Handles .tar.bz2, .tbz2
+        case application/gzip
+            tar xvzf $archive_file # Handles .tar.gz, .tgz
+        case application/x-xz
+            tar xvfJ $archive_file # Handles .tar.xz
+        case application/x-7z-compressed
+            7z x $archive_file
+        case application/x-compress
+            uncompress $archive_file
+        case application/zstd
+            unzstd $archive_file
+        case "*" # Default if no match is found
+            echo "Unknown file type: $file_type" >&2
+            return 1
     end
 end
